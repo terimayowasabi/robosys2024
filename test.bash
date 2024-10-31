12a0cc586026c5d201e812281f3034fe3409903d
terimayowasabi <tomo8279332@icloud.com>
2024-10-31 12:18:48 +0900

    -A

--- test.bash
+++ test.bash
@@ -7,8 +7,8 @@ ng () {
 
 res=0
 
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "LINENO"
 
+[ "${res}" = 0 ] && echo OK
 exit $res
