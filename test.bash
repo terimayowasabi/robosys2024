0263bbd6b093bb9de537f5636114429726c3e0e3
terimayowasabi <tomo8279332@icloud.com>
2024-10-31 13:01:41 +0900

    -A

--- test.bash
+++ test.bash
@@ -11,5 +11,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
+out=$(seq あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "${res}" = 0 ] && echo OK
 exit $res
