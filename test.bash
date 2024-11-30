ae26241757315bd7e6c5e8a4328c2142c7355902
terimayowasabi <tomo8279332@icloud.com>
2024-11-30 15:43:01 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -19,5 +19,9 @@ out=$(echo | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+out=$(cat nums | tr ' ' '\n' | ./analysis > ans)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
