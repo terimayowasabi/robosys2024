456fdc790e4cfa64030db67c40a0101c1b111445
terimayowasabi <tomo8279332@icloud.com>
2024-12-08 23:16:05 +0900

    analysistest

--- test.bash
+++ test.bash
@@ -19,7 +19,18 @@ out=$(echo | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(cat testng | tr ' ' '\n' | ./analysis > ans)
+out=$(seq 5 | ./analysis)
+[ "${out}" = [1, 2, 3, 4, 5]   中央値:3  平均値:3.0　分散:2.0  最大値:5  最小値:1 ] || ng "$LINENO"
+
+out=$(echo あ い う> nums | cat nums| tr ' ' '\n' | ./analysis > ans)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  > nums | cat nums| tr ' ' '\n' | ./analysis > ans)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo 1　2　3> nums | cat nums| tr ' ' '\n' | ./analysis > ans)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
